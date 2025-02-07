2e3124859e0ee2da429666990184c0fb30d9a879
Atanas Cholakov <116814210+AtanasCholakov@users.noreply.github.com>
2025-02-07 15:05:03 +0200

    firebase auth

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFunctions } from "firebase/functions";
 
 // Firebase конфигурация
 const firebaseConfig = {
-  apiKey: "AIzaSyCnfz2W91EIpWI0yB46zp5LuiCdtnrgxXA",
-  authDomain: "tripmate-b6bc4.firebaseapp.com",
-  projectId: "tripmate-b6bc4",
-  storageBucket: "tripmate-b6bc4.appspot.com",
-  messagingSenderId: "1068730370971",
-  appId: "1:1068730370971:web:05a4c5e1f58956869a6f3e",
-  measurementId: "G-Q4KGF5W3X1",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Инициализация на Firebase App
